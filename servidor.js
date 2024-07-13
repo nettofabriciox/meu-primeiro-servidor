c9d6c169a408225af603e4ba3302864d8abf3d3b
fabricionetto <agostinhonetto73@gmail.com>
2024-07-12 21:25:23 -0300

    Criando servidor

--- servidor.js
+++ servidor.js
@@ -1,1 +1,7 @@
-const http = require('http')
+const http = require('http');
+
+http.createServer((requisicao, resposta) =>{
+    resposta.writeHead(200,{'Content-Type':'text/plain'});
+    resposta.write('Ola sou servidor');
+    resposta.end()
+})
